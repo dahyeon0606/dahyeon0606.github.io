8ad88e6d164d15c6deec46ceba05b1a43069ee46
HaDaHyeon <hada3817@gmail.com>
2025-01-09 19:22:01 +0900

    first posting

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736405186',resources: [
+    cacheName: 'chirpy-1736417949',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
