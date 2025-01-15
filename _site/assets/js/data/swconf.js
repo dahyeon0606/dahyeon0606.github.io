250f57ee3b7b8341736e3e091f8f662656dcfe46
dahyeon0606 <hada3817@gmail.com>
2025-01-15 16:06:54 +0900

    post:git add img

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736923708',resources: [
+    cacheName: 'chirpy-1736924738',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
